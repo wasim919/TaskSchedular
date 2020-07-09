41ba196f4edce1820dcd0977afb4a6bda1945cce
wasim919 <wasimwik919@gmail.com>
2020-07-09 23:06:39 +0530

    reducers debugged

--- src/reducers/taskManager.js
+++ src/reducers/taskManager.js
@@ -1,59 +1,54 @@
 const initialState = {
-    servers: [],
-    tasks: [],
-    nextId: 0
-}
+  servers: [],
+  tasks: [],
+  nextId: 0,
+};
 
-const taskManager = (state = initialState, action) {
-    switch (action.type) {
-        
-        case 'ADD_SERVER':
-            
-            newServer = {
-                index: state.nextId,
-                serverAvailable: true
-            }
+const taskManager = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_SERVER':
+      newServer = {
+        index: state.nextId,
+        serverAvailable: true,
+      };
 
-            return {
-                ...state,
-                servers: [...servers, newServer],
-                nextId: nextId + 1
-            }
+      return {
+        ...state,
+        servers: [...servers, newServer],
+        nextId: nextId + 1,
+      };
 
-        case 'ADD_TASK':
-        
-            newTask = {
-                index: state.nextId,
-                taskCompleted: false,
-            };
-            
-            return {
-                ...state,
-                tasks: [...tasks, newTask],
-                nextId: nextId + 1 
-            }
-        
-        case 'REMOVE_TASK':
-            
-            const index = action.payload;
-            state.tasks.splice(index, 1);
-            
-            return {
-                ...state,
-                tasks: [...state.tasks]
-            }
-        
-        case 'REMOVE_SERVER':
-            
-            const index = action.payload;
-            state.servers.splice(index, 1);
+    case 'ADD_TASK':
+      newTask = {
+        index: state.nextId,
+        taskCompleted: false,
+      };
 
-            return {
-                ...state,
-                servers: [...state.servers]
-            }
+      return {
+        ...state,
+        tasks: [...tasks, newTask],
+        nextId: nextId + 1,
+      };
 
-        default:
-            return state;
-    }
-}
+    case 'REMOVE_TASK':
+      const index = action.payload;
+      state.tasks.splice(index, 1);
+
+      return {
+        ...state,
+        tasks: [...state.tasks],
+      };
+
+    case 'REMOVE_SERVER':
+      const index = action.payload;
+      state.servers.splice(index, 1);
+
+      return {
+        ...state,
+        servers: [...state.servers],
+      };
+
+    default:
+      return state;
+  }
+};
